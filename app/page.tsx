d1730ebaec4b68943c50490e5e222c83568ef8df
dhwanijain-dev <dhwanijain03121606@gmail.com>
2025-09-05 03:04:45 +0530

    intialization

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,177 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
+'use client'
 
-export default function Home() {
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@radix-ui/react-label'
+import React,{ useState } from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import supabase from './supabase-client'
+const page = () => {
+  const [newTask,setNewTask] = useState({title:'',description:''})
+  const [message,setMessage] = useState('')
+  const [todos,setTodos] = useState<any[]>([])
+  const [updateTodo,setUpdateTodo] = useState('')
+  const [updateDescription,setUpdateDescription] = useState('')
+ const [open, setOpen] = useState(false)
+ 
+ React.useEffect(() => {
+    fetchTodos();
+    
+  }, [todos])
+
+  const fetchTodos = async () => {
+    const {data,error} = await supabase
+    .from('todo')
+    .select('*')
+    if(error){
+      console.log(error)
+    }else{
+      setTodos(data)
+    }
+  }
+  
+
+  const handleSubmit = async (e:React.FormEvent) => {
+    e.preventDefault();
+    if(newTask.title === '' || newTask.description === ''){
+      setMessage('Please fill in all fields')
+      return;
+    }
+    const {data,error} = await supabase
+    .from('todo')
+    .insert([{todo:newTask.title,description:newTask.description}])
+    if(error){
+      setMessage('Error adding task')
+      console.log(error)
+    }else{
+      setMessage('Task added successfully')
+      setNewTask({title:'',description:''})
+    }
+  }
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
-            </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
-          </div>
-        </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-          </main>
-        </div>
+    <div className='flex flex-col justify-center items-center h-screen '>
+      
+<Card className="h-96 w-96 p-4 flex flex-col justify-evenly gap-3">
+  <h1 className='text-2xl font-bold text-center'>Welcome To My Todo </h1>
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
-      </div>
-    </main>
-  );
+    <div>
+      <form className=" flex flex-col gap-2">
+        <Label htmlFor='task'>New Todo</Label>
+          <Input type='text'
+          placeholder='Add a new todo...' 
+          onChange={(e) => setNewTask({...newTask, title: e.target.value})}
+          />
+          <Label htmlFor='taskDesc'>Todo Description</Label>
+          <Input type='text' 
+          placeholder='Add a task description...' 
+          onChange={(e) => setNewTask({...newTask, description: e.target.value})}
+          />
+          <Button type='submit' onClick={handleSubmit} className='mt-4'>Add Task</Button>
+          {message && <p className='text-green-500'>{message}</p>}
+      </form>
+
+      
+      
+
+    </div>
+
+
+</Card>
+    
+<Card className='w-1/2 mt-10'>
+{todos.length === 0 ? (
+  <p className='p-10 text-center'>No todos available</p>
+):(
+  <Table className='p-10'>
+
+  <TableCaption className='mb-3'>All Todos</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Todo</TableHead>
+      <TableHead>Description</TableHead>
+
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    {todos.map((todo) => (
+      <TableRow key={todo.id}>
+        <TableCell className="font-medium">{todo.todo}</TableCell>
+        <TableCell>{todo.description}</TableCell>
+        <TableCell className='flex items-center justify-end gap-2'>
+            <Button variant='destructive' onClick={async() => {
+                const {data,error} = await supabase
+                .from('todo')
+                .delete()
+                .eq('id',todo.id)
+                if(error){
+                  console.log(error)
+                }else{
+                  setMessage('Task deleted successfully')
+                }
+              }}>Delete</Button>
+
+        
+        <Dialog open={open} onOpenChange={setOpen}>
+              <DialogTrigger onClick={() => setOpen(true)}>Update</DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>This Will Edit Your Todo?</DialogTitle>
+                  <DialogDescription>
+                    This will update your todo to "Updated Task"
+                  </DialogDescription>
+                </DialogHeader>
+                <Input type='text' placeholder='update todo'
+                onChange={(e) => setUpdateTodo(e.target.value)}
+                />
+                <Input type='text' placeholder='update description'
+                onChange={(e) => setUpdateDescription(e.target.value)}
+                />
+                <Button variant='secondary' onClick={async() => {
+                const {data,error} = await supabase
+                .from('todo')
+                .update({todo: updateTodo, description: updateDescription})
+                .eq('id',todo.id)
+                if(error){
+                  console.log(error)
+                }else{
+                  setMessage('Task updated successfully')
+                  setOpen(false)
+                }
+              }}>Update</Button>
+
+              
+              </DialogContent>
+            </Dialog>
+
+        </TableCell>
+      </TableRow>
+    ))}
+  </TableBody>
+</Table>
+)
 }
+
+</Card>
+
+    </div>
+  )
+}
+
+export default page
